986a700943e676e14cbb9fda45834af7158db3ee
vhsed <119089396+vhsed@users.noreply.github.com>
2022-11-26 11:41:54 +0800

    Update addtrojan.sh

--- xray/addtrojan.sh
+++ xray/addtrojan.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/vhsec/izin/main/ip.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/vhsed/perizinan/main/ipvps.txt | grep $MYIP )
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
